5714c99d1113cc51696d3a775e2bd1aa94a5d839
grosser <michael@grosser.it>
2014-12-27 15:38:35 -0800

    initial

--- a-panzer.gemspec
+++ a-panzer.gemspec
@@ -1,11 +1,14 @@
-name = "a_panzer"
-require "./lib/#{name.gsub("-","/")}/version"
+name = "a-panzer"
+require "./lib/a_panzer/version"
 
 Gem::Specification.new name, APanzer::VERSION do |s|
-  s.summary = "A panzer game"
+  s.summary = "A. Panzer game"
   s.authors = ["Michael Grosser"]
   s.email = "michael@grosser.it"
   s.homepage = "https://github.com/grosser/#{name}"
   s.files = `git ls-files lib/ bin/ MIT-LICENSE`.split("\n")
   s.license = "MIT"
+  s.executables = ["a-panzer"]
+  s.required_ruby_version = '>= 2.0.0' # curses does not build on 1.9
+  s.add_runtime_dependency "dispel", ">= 0.0.7"
 end
--- lib/a_panzer.rb
+++ lib/a_panzer.rb
@@ -1,2 +1,76 @@
 module APanzer
+  class Game
+    PANZER = []
+    PANZER << <<-SPRITE
+
+\\
+|"""\\-=
+(____)
+    SPRITE
+    PANZER << <<-SPRITE
+         __,-.
+\\      ( .`-')
+|"""\\-=(_ (_,_)
+(____)   `--'
+    SPRITE
+    PANZER << <<-SPRITE
+         __,-.
+\\       ( .`-')_.o
+|"""\\-=(_ (_,_)
+(____)   `--'
+    SPRITE
+    PANZER << <<-SPRITE
+         ..,-.      _.--""""o
+\\       : .`-';_.-"
+|"""\\-=:. (.,.)
+(____)   `:-'
+    SPRITE
+    PANZER << <<-SPRITE
+         .. .       _.--""""--.
+\\       : . : ; . "             "-.
+|"""\\-=:. :.,.;                    `.
+(____)   `.:'                        o
+    SPRITE
+
+    def initialize
+      @position = 0
+      @sprite = 0
+      @pew = false
+      @moving = false
+    end
+
+    def board
+      PANZER[@sprite].split("\n").map { |l| "#{" " * @position}#{l}" }
+    end
+
+    def pew?
+      @pew
+    end
+
+    def moving?
+      @moving
+    end
+
+    def move(x)
+      return if @pew
+      @moving = x
+      animate
+    end
+
+    def shoot
+      @pew = true
+      animate
+    end
+
+    def animate
+      if @pew
+        @sprite += 1
+        @sprite %= PANZER.size
+        @pew = false if @sprite == 0
+      elsif @moving
+        @position += @moving
+        @moving = false if @position == 0
+      end
+    end
+  end
 end
